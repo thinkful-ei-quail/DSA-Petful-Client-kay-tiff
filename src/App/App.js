142534ff6fad128a7a2a5ea9861a6d84a1009eb3
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-02 15:47:25 -0500

    app state and didmounts

--- src/App/App.js
+++ src/App/App.js
@@ -8,13 +8,53 @@ import config from "../config";
 
 class App extends Component {
 
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         dogs: [],
-    //         cats: [],
-    //     };
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            dog: [],
+            cat: [],
+            people: [],
+        };
+    }
+
+    dogComponentDidMount() {//first dog
+        fetch(`${config.API_ENDPOINT}/pets/dog`)
+    
+        .then(response => response.json())
+        .then((dog) => {
+          console.log('dog', dog)
+          this.setState({dog });
+        })
+        .catch((error) => {
+          console.error(error.message );
+        });
+    }
+    catComponentDidMount() {//first cat
+        fetch(`${config.API_ENDPOINT}/pets/cat`)
+    
+        .then(response => response.json())
+        .then((cat) => {
+          console.log('cat', cat)
+          this.setState({cat });
+        })
+        .catch((error) => {
+          console.error(error.message );
+        });
+      }
+    queueComponentDidMount() {// people queue
+        fetch(`${config.API_ENDPOINT}/people`)
+    
+        .then(response => response.json())
+        .then((queue) => {
+          console.log('queue', queue)
+          this.setState({queue});
+        })
+        .catch((error) => {
+          console.error(error.message );
+        });
+    }
+    
+
 
     render() {
         return(
