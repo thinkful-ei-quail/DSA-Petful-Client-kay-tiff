4c7b05ca46039393b9d695dffdef6a0a9fca2621
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-03 18:22:52 -0400

    timer

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,27 @@
+// if user is adopting an animal
+// autopopulate list every 5 seconds until they're 5 people in line
+
 function incrementCount() {
     let counter = 0
     for (let i = 0; i <= 5000; i++) {
         counter++
     }
-      incrementCount()
+    incrementCount()
 }
 
-module.exports = {incrementCount}
+if (this.viewType === true) {
+    function incrementCount() {
+        let counter = 0
+        let arr = ['Jim', 'Joe', 'Jan', 'Jack', 'Bob']
+        for (let i = 0; i <= 5000; i++) {
+            submitPerson(arr[i])
+            counter++
+        }
+        if(counter === 6){
+            return
+        }
+        incrementCount()
+    }
+}
+
+module.exports = { incrementCount }
