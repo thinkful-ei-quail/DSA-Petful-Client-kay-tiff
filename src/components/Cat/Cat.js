423431a6fe444015a147b6bc9cb67e5537121a54
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-12 21:35:21 -0500

    tiff change

--- src/components/Cat/Cat.js
+++ src/components/Cat/Cat.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ApiContext from '../../ApiContext';
+import config from '../../config';
 
 class Cat extends Component {
     constructor(props) {
@@ -9,17 +10,46 @@ class Cat extends Component {
         };
     }
     static contextType = ApiContext;
-    render() {   
+
+    adoptCat = (e) => {
+        e.preventDefault()
+        fetch(`${config.API_ENDPOINT}pets/cat`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({type: 'cats'}),
+        })
+            .then(() => {
+                fetch(`${config.API_ENDPOINT}pets/cat`)
+            })
+        e.preventDefault()
+        fetch(`${config.API_ENDPOINT}people`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(),
+        })
+            .then(() => {
+                window.location.reload()
+            })
+    }
+
+    render() {
+     
         const {cat =[],  } = this.context;
+
         return(
             <div className='main-cat'>
-               <div className='btn'><button>Adopt A Cat</button></div>
+               
                <img src={cat.imageURL} alt='my headshot'/>
                <p>Name: {cat.name}</p>
                <p>Gender: {cat.gender}</p>
                <p>Age: {cat.age}</p>
                <p>Breed: {cat.breed}</p>
-               <p>{this.context.splitName(`${cat.name}`)}'s story: {cat.story}</p>
+               <p>{this.context.splitName(`${cat.name}`)}'s Story: {cat.story}</p>
+               <div className='btn'><button onClick={(e) => this.adoptCat(e)}>Adopt {cat.name}</button></div>
             </div>
         );
     }
