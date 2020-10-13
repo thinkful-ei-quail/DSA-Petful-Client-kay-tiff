0cfcef02e457844fd7b4e8760bedb98330ae2801
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-12 21:42:57 -0500

    tiff change

--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -1,29 +1,60 @@
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ApiContext from '../../ApiContext';
+import config from '../../config';
 
 class Dog extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            
+            name: {
+                value: "",
+            },
+            isError: false,
+            errorMsg: "",
+            redirect: null,
         };
     }
 
     static contextType = ApiContext;
 
+    adoptDog = (e) => {
+        e.preventDefault()
+        fetch(`${config.API_ENDPOINT}pets/dog`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({type: 'dog'}),
+        })
+        .then(() => {
+            fetch(`${config.API_ENDPOINT}pets/dog`)
+        })
+        
+        e.preventDefault()
+        fetch(`${config.API_ENDPOINT}people`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(),
+        })
+        .then(() => {
+            window.location.reload()
+        })
+    }
     render() {
         const {dog =[] } = this.context;
         return(
             <div className='main-dog'>
-               <div className='btn'><button>Adopt A Dog</button></div>
                <img className='img' src={dog.imageURL} alt='my headshot'/>
                <p>Name: {dog.name}</p>
                <p>Gender: {dog.gender}</p>
                <p>Age: {dog.age}</p>
                <p>Breed: {dog.breed}</p>
-               <p>{this.context.splitName(`${dog.name}`)}'s story: {dog.story}</p>
+               <p>{dog.name}'s Story: {dog.story}</p>
+               <div className='btn'><button onClick={(e) => this.adoptDog(e)}>Adopt {dog.name}</button></div>
             </div>
         );
     }
