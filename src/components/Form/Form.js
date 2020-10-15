12b92b28ecf02dcf8870df1a87bfc74c84ff9bdc
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-14 23:17:34 -0500

    create a recursive demo function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,9 +4,10 @@ import './Form.css'
 
 import config from '../../config';
 import ApiContext from '../../ApiContext';
+import { render } from '@testing-library/react';
 
 class Form extends Component {
-
+    
     constructor(props) {
         super(props)
         this.state = {
@@ -59,11 +60,11 @@ class Form extends Component {
                 return res.json();
             })
             .then(() => {
-                this.context.enqueue(this.state.name.value);
+               return this.context.enqueue(this.state.name.value);
             })
-            // .then(() =>{
-            //     this.runDemo();
-            // })
+            .then((event) =>{
+                this.runDemo(event);
+            })
             .catch((error) => {
                 this.setState({
                     isError: true,
@@ -72,49 +73,90 @@ class Form extends Component {
             })
         }
     }
-
+    
     updateName = (name) => {
         this.setState({ name: { value: name } })
     }
-
-    runDemo = () => {
-        if (this.context.userName === this.context.queue[0]){
-            let adoptees = ['Dolly Parton', 'Lucy Ball', 'Jenny From The Block', 'Samantha Adams', 'Chartreuse Brown', 'Michael Phelps', 'Christian Dior', 'Coco Chanel', 'Shay Evans', 'Mr.PotatoHead']
-            let timerFunc = setInterval(() => {
-        
-                fetch(`${config.API_ENDPOINT}people`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        person: adoptees[Math.floor((Math.random() * 10))],
-                    }),
-                })
-                
-                .then(() => {
-                    window.location.reload()
-                })
-                if (this.context.queue.length >=   5) {
-                    clearInterval(timerFunc)
-                }
-            }, 5000)
-            return
-        }else if (this.context.userName !== this.context.queue[0]){
-            fetch(`${config.API_ENDPOINT}people`,{
+    
+    
+    runDemo = (event) => { setTimeout(() => {  
+        if (this.state.name.value === this.context.queue[0] && this.context.queue.length === 5){
+            console.log('yay!')
+            clearTimeout(this.runDemo)
+            return window.location.reload()
+        } 
+        if (this.state.name.value !== this.context.queue[0] && this.context.queue.length > 1 ){
+            fetch(`${config.API_ENDPOINT}pets/dog`, {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify()
+                body: JSON.stringify({type: 'dog'}),
             })
-            .then(()=>{
-                window.location.reload()
+            fetch(`${config.API_ENDPOINT}people`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(),
+            }).then(() => {
+                clearTimeout(this.runDemo)
+            }).then(() => {
+                this.runDemo(event)
             })
-            return this.runDemo()
+            return
         }
-
-    }
+        if ( this.context.queue.length < 5 && this.state.name.value === this.context.queue[0] ){
+            let adoptees = ['Dolly Parton', 'Lucy Ball', 'Jenny From The Block', 'Samantha Adams', 'Chartreuse Brown', 'Michael Phelps', 'Christian Dior', 'Coco Chanel', 'Shay Evans', 'Mr.PotatoHead']
+            fetch(`${config.API_ENDPOINT}people`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    person: adoptees[Math.floor((Math.random() * 10))],
+                }),
+            }).then(() =>{
+                fetch(`${config.API_ENDPOINT}people`)
+            // }).then(()=> {
+            //     if (this.state.name.value !== this.context.queue[0]){
+            //         fetch(`${config.API_ENDPOINT}pets/dog`, {
+            //             method: "DELETE",
+            //             headers: {
+            //                 "Content-Type": "application/json",
+            //             },
+            //             body: JSON.stringify({type: 'dog'}),
+            //         })
+            //         fetch(`${config.API_ENDPOINT}people`, {
+            //             method: "DELETE",
+            //             headers: {
+            //                 "Content-Type": "application/json",
+            //             },
+            //             body: JSON.stringify(),
+            //         }).then(()=>{
+            //             if (this.context.queue.length < 5){
+            //                 fetch(`${config.API_ENDPOINT}people`, {
+            //                     method: "POST",
+            //                     headers: {
+            //                         "Content-Type": "application/json",
+            //                     },
+            //                     body: JSON.stringify({
+            //                         person: adoptees[Math.floor((Math.random() * 10))],
+            //                     }),
+            //                 }).then(() =>{
+            //                     fetch(`${config.API_ENDPOINT}people`)
+            //                 })
+            //             }
+            //         })
+            //     } 
+            }).then(() => {
+                clearTimeout(this.runDemo)
+            })
+            this.runDemo(event)
+            return
+        }
+    }, 5000)
+}
 
     render() {
         if (this.state.redirect) {
