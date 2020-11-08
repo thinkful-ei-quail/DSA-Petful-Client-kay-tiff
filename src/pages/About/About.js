3f3feb4973b84f1ab4d66ce0a8ba2493dd9f20fe
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-07 18:04:37 -0600

    about page image

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import {Link} from 'react-router-dom';
-import './About.css'
+import './About.css';
+import ApiContext from '../../ApiContext';
+import demo from '../../images/demo.jpg';
 class About extends Component {
+    static contextType = ApiContext;
     render() {
+        const displayImage = ()=>{
+            if (this.context.pets[0]){
+                return(
+                    <img className='img' src={this.context.pets[0].imageURL} alt='pet'/>
+                )
+            }else{
+                return(
+                    <img className='img' src={demo} alt='pet'/>
+                )
+            }
+        }
         return(
             <div className='container'>
                <Header/>
+               {displayImage()}
                <p>Petful is an adoption agency that encourages equity by adopting out cats and dogs by the length of time 
                    they've spent here. Each pet is adopted out on a first come first serve basis. To adopt a pet you must 'get in line' 
                    by signing up on our adoptee list, and then once its your turn to adopt, the oldest pet at that time is who you take 
@@ -17,7 +32,7 @@ class About extends Component {
                 </Link>
             </div>
         );
-    }
-}
+    };
+};
 
-export default About
+export default About;
